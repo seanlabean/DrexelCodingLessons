d39f291cdf2233cc12547ec666a7d17713207ce6
Sean Lewis <scl63@drexel.edu>
2020-08-25 23:12:51 -0400

    Addition of python_test.py cleaning up UnixSystem.txt

---
+++ Info/python_test.py
@@ -0,0 +1,12 @@
+print('Hello! I am going to try and import some Python libraries...')
+
+import numpy
+print('... imported numpy')
+import matplotlib.pyplot as plt
+print('... imported matplotlib')
+import mpi4py
+print('... imported mpi4py')
+import pandas
+print('... imported pandas')
+
+print('Success!! :D')
